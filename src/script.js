084ffe3c492eab74fe041bfc00e2451043ed51f8
Tetiana Paniot <tatyana.paniot@gmail.com>
2024-02-07 10:37:19 +0000

    initial files created

---
+++ src/script.js
@@ -0,0 +1,8 @@
+function printNumbersWithInterval() {
+    const numbersArray = [];
+    for (let i = 20; i <= 30; i += 0, 5) {
+        document.write(`${numbersArray[i]}`);
+    }
+}
+
+printNumbersWithInterval();
