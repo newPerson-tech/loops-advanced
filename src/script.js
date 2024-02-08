af34c5e46b285a6f5de36bb6c3e94ebd72383a3e
Tetiana Paniot <tatyana.paniot@gmail.com>
2024-02-08 21:48:29 +0000

    dicplaying function results on the DOM

--- src/script.js
+++ src/script.js
@@ -1,25 +1,21 @@
 //Task 1 Write a function to print numbers from 20 to 30 with the interval 0,5
 
-function printNumbersWithInterval() {
-    const numbersArray = [];
-    for (let i = 20; i <= 30; i += 0, 5) {
-        document.write(`${numbersArray[i]}`);
-    }
+
+const taskOne = document.getElementById('task-one');
+const numbersArray = [];
+for (let i = 20; i <= 30; i += 0, 5) {
+    taskOne.innerHTML = `${numbersArray[i]}`;
 }
 
-printNumbersWithInterval();
 
 
 //Task 2 Write a function to count exchange rate usd-uah
 
 const exchangeRate = 27;
 
-function countDollars() {
-    for (let i = 10; i <= 100; i++) {
-        const uah = i * exchangeRate;
-        const exchangeAmount = (uah / 100).toFixed(2);
-        document.write(`${i} uah = ${exchangeAmount}`);
-    }
+const taskTwo = document.getElementById('task-two');
+for (let i = 10; i <= 100; i++) {
+    const uah = i * exchangeRate;
+    const exchangeAmount = (uah / 100).toFixed(2);
+    taskTwo.innerHTML = `${i} uah = ${exchangeAmount}`;
 }
-
-countDollars();
