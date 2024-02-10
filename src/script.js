9132f735bf2286779c8fdab52fd460b11593ae43
Tetiana Paniot <tatyana.paniot@gmail.com>
2024-02-10 20:46:05 +0000

    square roots function done

--- src/script.js
+++ src/script.js
@@ -1,11 +1,11 @@
 //Task 1 Write a function to print numbers from 20 to 30 with the interval 0,5
 const taskOne = document.getElementById('task-one');
 const taskTwo = document.getElementById('task-two');
-const taskThree = document.getElementById('task-three');
+const taskThree = document.getElementById('task-three').value;
 const taskFour = document.getElementById('task-four');
 const taskFive = document.getElementById('task-five');
 
-taskOne.textContent = `Print numbers with interval 0.5:`;
+taskOne.textContent = `Task 1: Print numbers with interval 0.5:`;
 
 function printOutNumbers() {
     for (let i = 20; i <= 30; i += 0.5) {
@@ -17,7 +17,7 @@ printOutNumbers();
 
 
 //Task 2 Write a function to count exchange rate usd-uah
-document.write(`Display exchange amounts for summs from 10 to 100, tens only, if 1 usd = 27 uah`);
+document.write(`Task 2: Display exchange amounts for summs from 10 to 100, tens only, if 1 usd = 27 uah`);
 
 function countExchangeAmount() {
     const exchangeRate = 27;
@@ -33,7 +33,16 @@ countExchangeAmount();
 //Task 3 There is number N given. Display all numbers square root of wich is not bigger than a saure root of N number
 
 
+function displayNumbersBasedOnSquareRoots(n) {
+    for (let i = 1; i <= 100; i++) {
+        if (Math.sqrt(i) < n) {
+            console.log(i);
+        }
+    }
+}
+
+const n = 10;
+displayNumbersBasedOnSquareRoots(n);
 
 
-
-
+//Task 4: 
