af82e4f4413d5ee1fe6f05fdef9f8afffbfea5e1
Tetiana Paniot <tatyana.paniot@gmail.com>
2024-02-10 21:29:32 +0000

    5 tasks on loops done

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ const taskThree = document.getElementById('task-three').value;
 const taskFour = document.getElementById('task-four');
 const taskFive = document.getElementById('task-five');
 
-taskOne.textContent = `Task 1: Print numbers with interval 0.5:`;
+taskOne.textContent = `Task 1. Print numbers with interval 0.5:`;
 
 function printOutNumbers() {
     for (let i = 20; i <= 30; i += 0.5) {
@@ -17,7 +17,7 @@ printOutNumbers();
 
 
 //Task 2 Write a function to count exchange rate usd-uah
-document.write(`Task 2: Display exchange amounts for summs from 10 to 100, tens only, if 1 usd = 27 uah`);
+document.write(`Task 2. Display exchange amounts for summs from 10 to 100, tens only, if 1 usd = 27 uah`);
 
 function countExchangeAmount() {
     const exchangeRate = 27;
@@ -32,7 +32,7 @@ countExchangeAmount();
 
 //Task 3 There is number N given. Display all numbers square root of wich is not bigger than a saure root of N number
 
-
+const n = 10;
 function displayNumbersBasedOnSquareRoots(n) {
     for (let i = 1; i <= 100; i++) {
         if (Math.sqrt(i) < n) {
@@ -41,8 +41,45 @@ function displayNumbersBasedOnSquareRoots(n) {
     }
 }
 
-const n = 10;
 displayNumbersBasedOnSquareRoots(n);
 
 
-//Task 4: 
+//Task 4: Integer x is given. Write a function to check if x is a primer number. 
+
+const x = 12;
+function isPrimeNumber(x) {
+    if (x <= 1) {
+        return false;
+    }
+    for (let i = 0; i <= Math.sqrt(x); i++) {
+        if (x % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const result = isPrimeNumber(x);
+console.log(result);
+
+taskFour.textContent = `Task 4. Is number given a primer number (true) or not (false): ${result}`;
+
+//Task 5: Write a function to check if a given number can be obtained by raising number 3 in any power
+
+const number = 81;
+function isPowerOfThree(x) {
+    if (x <= 0) {
+        return false;
+    }
+
+    while (x % 3 === 0) {
+        x /= 3;
+    }
+    return x === 1;
+}
+
+const functionResult = isPowerOfThree(number);
+
+console.log(functionResult);
+
+taskFive.textContent = `Task 5. Is number given can be obtaine raising 3 in any power? (true) or not (false): ${result}`;
